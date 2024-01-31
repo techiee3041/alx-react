5bac5927ad1ffb8a5dab11c7288d6b9ad0ec72f1
techiee3041 <doreenoms6699@gmail.com>
2024-01-31 07:51:39 +0300

    height: 100vh;

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -14,10 +14,10 @@ function App() {
       <div className="App-body">
         <p>Login to access the full dashboard</p>
 
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email:</label>
         <input type="email" name="email" id="email" />
 
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">Password:</label>
         <input type="password" name="password" id="password" />
 
         <button type="submit">OK</button>
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -2,4 +2,8 @@ const getFullYear = () => new Date().getFullYear();
 
 const getFooterCopy = (isIndex) => (isIndex ? 'Holberton School' : 'Holberton School main dashboard');
 
-export { getFullYear, getFooterCopy };
+const getLatestNotification = () => {
+    return `<strong>Urgent requirement</strong> - complete by EOD`
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification };
