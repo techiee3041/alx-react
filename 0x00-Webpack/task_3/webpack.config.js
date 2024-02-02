b942e7b704a6644a0b1ba40024b759a3dd3597ac
techiee3041 <doreenoms6699@gmail.com>
2024-02-02 23:24:49 +0300

    adjust the web-server config in package.json

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -36,7 +36,9 @@ module.exports = {
     },
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
     open: true,
     port: 8564,
   },
