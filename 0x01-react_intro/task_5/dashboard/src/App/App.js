9b0be3b1d97f4cf6c6a3beca4dbf27719af0a579
techiee3041 <doreenoms6699@gmail.com>
2024-02-02 23:08:43 +0300

    @

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,7 @@
-import logo from "./holberton-logo.jpg";
+import React from "react";
+import logo from "../assets/holberton-logo.jpg";
 import "./App.css";
-import { getFullYear, getFooterCopy } from "./utils";
+import { getFullYear, getFooterCopy } from "../utils/utils";
 
 function App() {
   return (
